482ed60427aae52a6c778e8c1013e735f306b05a
Rádio Raio Vida <neyrigalarce@gmail.com>
2026-01-13 20:59:34 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAQiqAAaOuVF-WyRDpJlp24emWvIDWr

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,10 @@
 // The default configuration is for a fake project.
 // You can't change the configuration for the fake project, but you can create your own project and use it here.
 export const firebaseConfig = {
-  "projectId": "adkairos-a739b",
-  "appId": "1:931281835933:web:9685a735b62b1ad6276856",
-  "storageBucket": "adkairos-a739b.appspot.com",
-  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  "authDomain": "adkairos-a739b.firebaseapp.com",
-  "messagingSenderId": "931281835933"
+  apiKey: "AIzaSyAQiqAAaOuVF-WyRDpJlp24emWvIDWrcUE",
+  authDomain: "studio-3817843906-26562.firebaseapp.com",
+  projectId: "studio-3817843906-26562",
+  storageBucket: "studio-3817843906-26562.appspot.com",
+  messagingSenderId: "740547620750",
+  appId: "1:740547620750:web:1e4be98ee475f2230b3edf"
 };
-
-    

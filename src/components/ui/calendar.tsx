77f55389b1b2da438f3dceebd19b655b42c72c70
Rádio Raio Vida <neyrigalarce@gmail.com>
2026-01-14 19:13:28 +0000

    Preciso que arrume os calendários do cadastro pois não dá pra incluir a

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -27,7 +27,7 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium hidden",
+        caption_label: "text-sm font-medium",
         caption_dropdowns: "flex justify-center gap-2",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
@@ -83,7 +83,7 @@ function Calendar({
                 handleChange(value)
               }}
             >
-              <SelectTrigger className="pr-1.5 focus:ring-0 w-[80px]">
+              <SelectTrigger className="pr-1.5 focus:ring-0 w-fit">
                 <SelectValue>{selected?.props?.children}</SelectValue>
               </SelectTrigger>
               <SelectContent position="popper">
